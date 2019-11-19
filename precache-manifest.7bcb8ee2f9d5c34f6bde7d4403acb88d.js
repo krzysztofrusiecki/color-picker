11ceca29e2bde7efaed1d65295737af804914088
krzysztofrusiecki <krzysztofrusiecki97@gmail.com>
2019-11-19 20:01:25 +0100

    Updates

---
+++ precache-manifest.7bcb8ee2f9d5c34f6bde7d4403acb88d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e85ee8b67e1f922329f86135145219c",
+    "url": "/color-picker/./index.html"
+  },
+  {
+    "revision": "592dd358682c69a2bfea",
+    "url": "/color-picker/./static/css/main.4e83c5e2.chunk.css"
+  },
+  {
+    "revision": "cc4ca55a40d6c5ce28af",
+    "url": "/color-picker/./static/js/2.13654fea.chunk.js"
+  },
+  {
+    "revision": "592dd358682c69a2bfea",
+    "url": "/color-picker/./static/js/main.63d0fe9c.chunk.js"
+  },
+  {
+    "revision": "0e9587ac3630f3d51027",
+    "url": "/color-picker/./static/js/runtime-main.d83c6eab.js"
+  }
+]);
